27f1cd34b2de7037780fbff6f606e191c1dd6a80
acevikel <acevikel@ku.edu.tr>
2019-10-01 21:43:00 -0700

    Revert "[WINB-21] Created skeleton for components"
    This reverts commit a85e67ce582b5cb317ae0b631afd470782d4764b.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,24 +8,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { ProjectTabComponentComponent } from './components/project-tab-component/project-tab-component.component';
-import { ProjectWinsComponentComponent } from './components/project-wins-component/project-wins-component.component';
-import { ProjectBenefitsComponentComponent } from './components/project-benefits-component/project-benefits-component.component';
-import { ProjectPriorizationComponentComponent } from './components/project-priorization-component/project-priorization-component.component';
-import { ProjectSettingsComponentComponent } from './components/project-settings-component/project-settings-component.component';
-import { ProjectComponentComponent } from './components/project-component/project-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponentComponent,
-    NavigationComponentComponent,
-    ProjectTabComponentComponent,
-    ProjectWinsComponentComponent,
-    ProjectBenefitsComponentComponent,
-    ProjectPriorizationComponentComponent,
-    ProjectSettingsComponentComponent,
-    ProjectComponentComponent
+    NavigationComponentComponent
   ],
   imports: [
     BrowserModule,
