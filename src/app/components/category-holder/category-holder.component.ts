156152286c8835b9a7a8f6cf83f680b135fbb156
Alex Hahn <AlexHahn15@gmail.com>
2019-10-02 19:47:09 -0700

    create category checkboxes; tested with dummy data [WINB-19]

--- src/app/components/category-holder/category-holder.component.ts
+++ src/app/components/category-holder/category-holder.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { categories } from './dummyCategories';
+
 @Component({
   selector: 'app-category-holder',
   templateUrl: './category-holder.component.html',
   styleUrls: ['./category-holder.component.css']
 })
 export class CategoryHolderComponent implements OnInit {
+  categories = categories;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
   }
