79c545fc03a774900388decb0c7ea444730cc12c
Alvaro Andres Pinzon Cortes <companycentralinfo@gmail.com>
2019-10-03 10:51:59 -0700

    Add temporal WinConditions to WinHolder component for testing

--- src/app/components/win-holder/win-holder.component.ts
+++ src/app/components/win-holder/win-holder.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { WinCondition } from '../../classes/win-condition';
 
 @Component({
   selector: 'app-win-holder',
@@ -9,10 +10,15 @@ export class WinHolderComponent implements OnInit {
 
   sortStates;
   currentSortState;
+  winConditions;
 
   constructor() {
     this.sortStates = ["Sort1", "Sort2"];
     this.currentSortState = "Sort1";
+    this.winConditions = [new WinCondition(["1","2"], ["1","2"], 1, "Carlos", 1, 1, ["UI"], "win condition text 1", [])];
+    this.winConditions.push(new WinCondition(["1","2"], ["1","2"], 2, "Carlos", 2, 2, ["UI"], "win condition text 2", []));
+    this.winConditions.push(new WinCondition(["1","2"], ["1","2"], 3, "Carlos", 3, 3, ["UI"], "win condition text 3", []));
+    console.log(this.winConditions[0]);
   }
 
   ngOnInit() {
