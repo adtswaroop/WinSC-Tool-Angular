946b3c246b7a1bb7cc7decbc37316c6abc8d9cb1
Alvaro Andres Pinzon Cortes <companycentralinfo@gmail.com>
2019-10-02 20:00:50 -0700

    Add initial properties and functions to sort-dropdown component

--- src/app/components/sort-dropdown/sort-dropdown.component.ts
+++ src/app/components/sort-dropdown/sort-dropdown.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-sort-dropdown',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SortDropdownComponent implements OnInit {
 
+  sortStates;
+  @Input() currentSortState: string;
+  @Output() currentSortStateChange = new EventEmitter<string>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  changeSortState() {
   }
 
 }
