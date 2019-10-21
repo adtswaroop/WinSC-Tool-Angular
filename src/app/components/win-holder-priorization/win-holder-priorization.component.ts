191ee8b22fa06c77b98755a0fdc353dbacdb3953
Alvaro Andres Pinzon Cortes <companycentralinfo@gmail.com>
2019-10-20 20:12:09 -0700

    Add the win conditions priorization list to the win-holder-priorization

--- src/app/components/win-holder-priorization/win-holder-priorization.component.ts
+++ src/app/components/win-holder-priorization/win-holder-priorization.component.ts
@@ -19,9 +19,10 @@ export class WinHolderPriorizationComponent implements OnInit {
   sortByLeastPriority(pWinConditions) {
     var clonePWinConditions = pWinConditions.slice(0);
     clonePWinConditions.sort(function(a, b){
-      var aTotalVotes = a.upVoters.length - a.downVoters.length;
-      var bTotalVotes = b.upVoters.length - b.downVoters.length;
-      return bTotalVotes - aTotalVotes;
+      
+      var aPriority = 0; //Modify this to take the priority of a
+      var bPriority = 0; //Modify this to take the priority of b
+      return bPriority - aPriority;
     });
     return clonePWinConditions;
   }
@@ -29,21 +30,15 @@ export class WinHolderPriorizationComponent implements OnInit {
   sortByHighestPriority(pWinConditions) {
     var clonePWinConditions = pWinConditions.slice(0);
     clonePWinConditions.sort(function(a, b){
-      var aTotalVotes = a.upVoters.length - a.downVoters.length;
-      var bTotalVotes = b.upVoters.length - b.downVoters.length;
-      return aTotalVotes - bTotalVotes;
+      var aPriority = 0; //Modify this to take the priority of a
+      var bPriority = 0; //Modify this to take the priority of b
+      return aPriority - bPriority;
     });
     return clonePWinConditions;
   }
 
   sort(currentSortStateChange) {
-    this.currentSortState = currentSortStateChange;
-    if(currentSortStateChange == "MostLikes"){
-      //this.winConditions = this.sortByMostLikes(this.winConditions);
-    }
-    else if(currentSortStateChange == "LeastLikes"){
-      //this.winConditions = this.sortByLeastLikes(this.winConditions);
-    }
+    
   }
 
 }
