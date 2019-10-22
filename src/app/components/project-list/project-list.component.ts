ca4ac3ab6f346441d992c2efe556aeedb5d008c6
YichengZheng <yichengzheng988@gmail.com>
2019-10-22 12:02:08 -0700

    HTML & CSS implementation for project list

---
+++ src/app/components/project-list/project-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { DummyData } from './../../classes/dummy-data';
+
+@Component({
+  selector: 'app-project-list',
+  templateUrl: './project-list.component.html',
+  styleUrls: ['./project-list.component.css']
+})
+export class ProjectListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
