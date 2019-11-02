965e837e313f9c7222960d9ef416951ec7ceec59
Alp Cevikel <acevikel@usc.edu>
2019-11-01 19:22:03 -0700

    Auth service added

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,16 +1,17 @@
-import { environment } from './../../environments/environment';
+import { BackendService } from './backend.service';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { User } from './../classes/user';
 
+
 @Injectable({ providedIn: 'root' })
 export class AuthenticationService {
     private currentUserSubject: BehaviorSubject<User>;
     public currentUser: Observable<User>;
 
-    constructor(private http: HttpClient) {
+    constructor(private http: HttpClient, private backend:BackendService) {
         this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
         this.currentUser = this.currentUserSubject.asObservable();
     }
@@ -20,13 +21,27 @@ export class AuthenticationService {
     }
 
     login(email, password) {
-        return this.http.post<any>(`${environment.urlBase}/user/login`, { email, password }) // TODO: move this to backend service
+        return  this.backend.login({email,password})
             .pipe(map(user => {
                 // store user details and jwt token in local storage to keep user logged in between page refreshes
                 localStorage.setItem('currentUser', JSON.stringify(user));
                 this.currentUserSubject.next(user);
                 return user;
             }));
+    }
+
+    register(email,password,firstName,lastName){
+        const token = '';
+        const id  = 0;
+        const type = "regular"; // TODO: Get user from backend once created
+        const userObj = {id,email,password,firstName,lastName,type,token}
+        return this.backend.register(userObj)
+          .pipe(map(token => {
+                userObj.token = token;
+                localStorage.setItem('currentUser', JSON.stringify(userObj));
+                this.currentUserSubject.next(userObj);
+                return userObj;
+          }));
     }
 
     logout() {
