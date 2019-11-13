faa51f01fa1f412ba2041088adef3d374bf2a492
Alp Cevikel <acevikel@usc.edu>
2019-11-12 17:44:43 -0800

    Votes are handled

--- src/app/services/backend.service.ts
+++ src/app/services/backend.service.ts
@@ -23,4 +23,24 @@ export class BackendService {
   getAllWinConditions(projectId) {
     return this.http.get<Array<WinCondition>>(`${environment.urlBase}/project/${projectId}/winconditions`);
   }
+
+  voteWinCondition(winConditionId:number ,voteType:number) {
+    if (voteType==1) {
+      return this.http.get<any>(`${environment.urlBase}/wincondition/${winConditionId}/upvote`);
+    } else if (voteType==-1){
+      return this.http.get<any>(`${environment.urlBase}/wincondition/${winConditionId}/downvote`);
+    } else if (voteType==0) {
+      return this.http.get<any>(`${environment.urlBase}/wincondition/${winConditionId}/unvote`);
+    }
+  }
+
+  voteWinConditionComment(commentId:number ,voteType:number) {
+    if (voteType==1) {
+      return this.http.get<any>(`${environment.urlBase}/winconditioncomment/${commentId}/upvote`);
+    } else if (voteType==-1){
+      return this.http.get<any>(`${environment.urlBase}/winconditioncomment/${commentId}/downvote`);
+    } else if (voteType==0) {
+      return this.http.get<any>(`${environment.urlBase}/winconditioncomment/${commentId}/unvote`);
+    }
+  }
 }
--- src/app/services/wincondition.service.ts
+++ src/app/services/wincondition.service.ts
@@ -13,20 +13,41 @@ export class WinconditionService {
 
 
   constructor(private backendService: BackendService) {
-    this.updateWinConditions(); //TODO: Fix this, we should parse all projects etc...
+    //this.updateWinConditions(); //TODO: Fix this, we should parse all projects etc...
     console.log("Wincondition constructor called");
   }
 
   updateWinConditions(){
-    const obj = this.backendService.getAllWinConditions(1);
+    const obj = this.backendService.getAllWinConditions(2);
     obj.subscribe((data) => {
       /*
       const winConditionArray = data.map((elem)=> {
           return new WinCondition(elem);
       });
       */
+     console.log(data);
       this.winConditionSource.next(data);
     });
     return obj;
+  }
+
+  handleVote(winConditionId:number , commentId:number, voteType:number) {
+    if (winConditionId && commentId) {
+      console.error("Critical Error! Vote component should have either winconditionID or commentId, not BOTH" );
+      return false;
+    }
+    let obj;
+    if (winConditionId) {
+        obj = this.backendService.voteWinCondition(winConditionId,voteType);
+    } else if (commentId) {
+        obj = this.backendService.voteWinConditionComment(commentId,voteType);
+    } else{
+      console.error("Critical error! Vote object doesn't have winconditionId or commentId");
+      return false;
+    }
+    obj.subscribe((data) => {
+      console.log("Vote response: "+data);
+    })
+
   }
 }
