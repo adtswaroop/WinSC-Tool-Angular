4fe8f33a31288676f02c82386b8505e8adbd6c24
acevikel <acevikel@ku.edu.tr>
2019-11-17 21:35:38 -0800

    merge routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,9 +3,35 @@ import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 import { MainComponent } from './components/main/main.component';
 import { Routes, RouterModule } from '@angular/router';
+import { ProjectListComponent } from './components/project-list/project-list.component';
+import { ProjectComponent } from './components/project/project.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 const routes: Routes = [
-  { path: '', component: MainComponent, canActivate: [AuthGuard] },
+  { path: '', component: MainComponent, canActivate: [AuthGuard], children:[
+    {
+      path: 'project-list',
+      component: ProjectListComponent
+    },
+    {
+      path: 'project',
+      component: ProjectComponent
+    },
+    {
+      path: 'profile',
+      component: ProfileComponent
+    },
+    {
+      path: '',
+      redirectTo: '/project-list',
+      pathMatch: 'full'
+    },
+    {
+      path: '**',
+      redirectTo: '/project-list',
+      pathMatch: 'full'
+    }
+  ] },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
 
