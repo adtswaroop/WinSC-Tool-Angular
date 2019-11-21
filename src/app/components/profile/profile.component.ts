9b5771fbecf5391d78433ed44c4318930a5d50a0
Alex Hahn <AlexHahn15@gmail.com>
2019-11-20 23:26:42 -0800

    [WINB-142] get photo file from user. Not connected to backend yet

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
+  url = null;
+
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
+  onPhotoUpload(event) {
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+
+      reader.readAsDataURL(event.target.files[0]);
+      reader.onload = (event) => {
+        this.url = event.target.result;
+      }
+    }
+  }
 }
