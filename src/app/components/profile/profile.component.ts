9bafd7e6879097612ac7fbb2824af8031e073813
Alex Hahn <alexhahn15@gmail.com>
2019-12-02 21:37:44 -0800

    [WINB-158] enable/disable buttons based on validity of form input

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -27,18 +27,48 @@ export class ProfileComponent implements OnInit, OnDestroy {
   ngOnInit() { 
     $(document).ready(function () {
       validate();
-      $('#firstNameInput, #lastNameInput, #emailInput').change(validate);
+      $('#firstNameInput, #lastNameInput, #emailInput').keyup(validate);
+      validatePW();
+      $('#passwordInput, #passwordConfirm').keyup(validatePW);
+      validateEmail();
+      $('#emailInput').keyup(validateEmail);
     });
   
     function validate() {
-      if ($('#firstNameInput').val().length > 0 ||
-          $('#lastNameInput').val().length > 0 ||
-          $('#emailInput').val().length > 0) {
-            $('#saveChanges').prop("disabled", false);
-            $('#cancelChanges').prop("disabled", false);
+      if ($('#firstNameInput').val().length > 0 || $('#lastNameInput').val().length > 0 || $('#emailInput').val().length > 0) {
+        $('#saveChanges').prop("disabled", false);
+        $('#cancelChanges').prop("disabled", false);
       } else {
         $('#saveChanges').prop("disabled", true);
         $('#cancelChanges').prop("disabled", true);
+      }
+    }
+
+    function validateEmail() {
+      var testEmail = /^[A-Z0-9._%+-]+@([A-Z0-9-]+\.)+[A-Z]{2,}$/i;
+      if ($('#emailInput').val().length != 0 && !testEmail.test($('#emailInput').val())) {
+        document.getElementById("email-warning").innerHTML = "Please enter a valid email address.";
+        $('#saveChanges').prop("disabled", true);
+      } else {
+        document.getElementById("email-warning").innerHTML = "";
+      }
+    }
+
+    function validatePW() {
+      if ($('#passwordInput').val().length > 0 || $('#passwordConfirm').val().length > 0) {
+        if ($('#passwordInput').val() == $('#passwordConfirm').val()) {
+          document.getElementById("password-warning").innerHTML = "";
+          $('#updatePassword').prop("disabled", false);
+          $('#cancelPassword').prop("disabled", false);
+        } else {
+          document.getElementById("password-warning").innerHTML = "Passwords do not match.";
+          $('#updatePassword').prop("disabled", true);
+          $('#cancelPassword').prop("disabled", false);
+        }
+      } else {
+        document.getElementById("password-warning").innerHTML = "";
+        $('#updatePassword').prop("disabled", true);
+        $('#cancelPassword').prop("disabled", true);
       }
     }
   }
@@ -49,16 +79,8 @@ export class ProfileComponent implements OnInit, OnDestroy {
 
       reader.readAsDataURL(event.target.files[0]);
       reader.onload = (event:any) => {
-          this.url = event.target.result;
+        this.url = event.target.result;
       }
-    }
-  }
-
-  confirmPassword(event, box) {
-    if((<HTMLInputElement> document.getElementById("passwordInput")).value !=  event.target.value) {
-      document.getElementById("password-warning").innerHTML = "Password doesn't match";
-    } else {
-      document.getElementById("password-warning").innerHTML = "";
     }
   }
 
